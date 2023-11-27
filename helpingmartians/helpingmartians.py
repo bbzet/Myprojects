c8ab482335083a2b665f6d40d7b6c9d433c035ef
Baiastan <baastan569@gmail.com>
2023-11-28 01:46:51 +0600

    Added to check if inputed data out of range of marked cargo locations(7 kilometers). Cargo's mark should inputed by not index(corrected).

--- helpingmartians/helpingmartians.py
+++ helpingmartians/helpingmartians.py
@@ -1,13 +1,26 @@
 import random
+
 distance = [200, 0, 200, 1, 4, 313, 9]
+
+
 first_data1 = distance[0]
 first_data2 = distance[2]
 first_data3 = distance[5]
 weight = 0
 
-
 for _ in range(3):
-    kilometers = int(input("Enter the kilometer mark where the box can be buried: "))
+    valid_input = False
+    while not valid_input:
+        try:
+            kilometers1 = int(input("Enter the kilometer mark where the box can be buried: "))
+            if 1 <= kilometers1 <= 7:
+                valid_input = True
+            else:
+                print("Enter numbers between 1 to 7.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+    kilometers = kilometers1 - 1
     weight += distance[kilometers]
 
 if weight == 713:
