02eddccafb80784a87f8cbc060689695326d7abd
Baiastan <baastan569@gmail.com>
2023-11-27 12:41:04 +0600

    Added inputed data by user to find cargo

--- helpingmartians/helpingmartians.py
+++ helpingmartians/helpingmartians.py
@@ -3,3 +3,6 @@ first_data1 = distance[0]
 first_data2 = distance[2]
 first_data3 = distance[5]
 weight = 0
+
+for _ in range(3):
+    kilometers = int(input("Enter the kilometer mark where the box can be buried: "))
