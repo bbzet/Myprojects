80470d374d782d2cc43fe5f2da16b65489570259
Baiastan <baastan569@gmail.com>
2023-11-29 09:48:02 +0600

    Added cycle for inputed data until user find the mark of cargo

--- helpingmartians/helpingmartians.py
+++ helpingmartians/helpingmartians.py
@@ -8,25 +8,28 @@ first_data2 = distance[2]
 first_data3 = distance[5]
 weight = 0
 
-for _ in range(3):
-    valid_input = False
-    while not valid_input:
-        try:
-            kilometers1 = int(input("Enter the kilometer mark where the box can be buried: "))
-            if 1 <= kilometers1 <= 7:
-                valid_input = True
-            else:
-                print("Enter numbers between 1 to 7.")
-        except ValueError:
-            print("Please enter a valid number.")
 
-    kilometers = kilometers1 - 1
-    weight += distance[kilometers]
+while True:
+    for _ in range(3):
+        valid_input = False
+        while not valid_input:
+            try:
+                kilometers1 = int(input("Enter the kilometer mark where the box can be buried: "))
+                if 1 <= kilometers1 <= 7:
+                    valid_input = True
+                else:
+                    print("Enter numbers between 1 to 7.")
+            except ValueError:
+                print("Please enter a valid number.")
 
-if weight == 713:
-    print(f"Congratulations! The weight is {weight} kilograms. You have marked the right locations of cargo.")
-else:
-    print(f'{weight} kilograms are not the right weight. You marked wrong locations, and the boxes changed their locations. Try it again.')
-    random.shuffle(distance)
+        kilometers = kilometers1 - 1
+        weight += distance[kilometers]
 
-    weight = 0
+    if weight == 713:
+        print(f"Congratulations! The weight is {weight} kilograms. You have marked the right locations of cargo.")
+        False
+    else:
+        print(f'{weight} kilograms are not the right weight. You marked wrong locations, and the boxes changed their locations. Try it again.')
+        random.shuffle(distance)
+
+        weight = 0
