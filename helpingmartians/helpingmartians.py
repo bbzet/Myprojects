a95b489e57b493833877fdd009056e34dffa6791
Baiastan <baastan569@gmail.com>
2023-11-30 14:57:41 +0600

    Added function and commentaries

--- helpingmartians/helpingmartians.py
+++ helpingmartians/helpingmartians.py
@@ -1,13 +1,21 @@
 import random
-
+#The first data where cargo were hidden
 distance = [200, 0, 200, 1, 4, 313, 9]
 
-
+#The first locations by index
 first_data1 = distance[0]
 first_data2 = distance[2]
 first_data3 = distance[5]
 weight = 0
 
+#Function that checked the weight
+def checking_mark(weight):
+    if weight == 713:
+        return "YES"
+    else:
+        return "NO"
+
+#Inputed data and checked operations
 r = True
 while r:
     for _ in range(3):
@@ -24,11 +32,11 @@ while r:
 
         kilometers = kilometers1 - 1
         weight += distance[kilometers]
-
-    if weight == 713:
+#The final result
+    if checking_mark(weight) == "YES":
         print(f"Congratulations! The weight is {weight} kilograms. You have marked the right locations of cargo.")
         r = False
-    else:
+    elif checking_mark(weight) == "NO":
         print(f'{weight} kilograms are not the right weight. You marked wrong locations, and the boxes changed their locations. Try it again.')
         random.shuffle(distance)
 
